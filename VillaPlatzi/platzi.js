7071dd7b650d9b1cdda94256fe8c174cd4ca7598
Richard Almonte <richard_almonte@hotmail.com>
2019-01-03 02:33:44 +0100

    Added all images

--- VillaPlatzi/platzi.js
+++ VillaPlatzi/platzi.js
@@ -1,25 +1,39 @@
 var vp = document.getElementById("villaPlatzi");
 var drawerContext = vp.getContext("2d");
 
-var mapTileImg = "img/tile.png";
+var tile = {
+    url: "img/tile.png",
+    loaded: false
+};
 
-var mapControl = new Image();
-mapControl.src = mapTileImg;
-mapControl.addEventListener("load", draw);
+var cow = {
+    url: "img/cow.png",
+    loaded: false
+};
+var pig = {
+    url: "img/pig.png",
+    loaded: false
+};
+var chick = {
+    url: "img/chick.png",
+    loaded: false
+};
 
+tile.image = new Image();
+tile.image.src = tile.url;
+tile.image.addEventListener("load", loadTile);
 
-var cowControl = new Image();
-cowControl.src = "img/cow.png";
-cowControl.addEventListener("load", draw);
+cow.image = new Image();
+cow.image.src = cow.url;
+cow.image.addEventListener("load", loadCow);
 
-var pigControl = new Image();
-pigControl.src = "img/pig.png"
-pigControl.addEventListener("load", draw);
+pig.image = new Image();
+pig.image.src = pig.url;
+pig.image.addEventListener("load", loadPig);
 
-
-var chickControl = new Image();
-chickControl.src = "img/chick.png"
-chickControl.addEventListener("load", draw);
+chick.image = new Image();
+chick.image.src = chick.url;
+chick.image.addEventListener("load", loadChick);
 
 
 function getRandom(min, max) {
@@ -27,6 +41,35 @@ function getRandom(min, max) {
     return result;
 }
 
+function loadTile() {
+    tile.loaded = true;
+    draw();
+}
+
+function loadCow() {
+    cow.loaded = true;
+    draw();
+}
+function loadPig() {
+    pig.loaded = true;
+    draw();
+}
+function loadChick() {
+    chick.loaded = true;
+    draw();
+}
+
 function draw() {
-    drawerContext.drawImage(mapControl, 0, 0);
+    if (tile.loaded) {
+        drawerContext.drawImage(tile.image, 0, 0);
+    }
+    if (cow.loaded) {
+        drawerContext.drawImage(cow.image, 10, 10);
+    }
+    if (pig.loaded) {
+        drawerContext.drawImage(pig.image, 100, 100);
+    }
+    if (chick.loaded) {
+        drawerContext.drawImage(chick.image, 350, 250);
+    }
 }

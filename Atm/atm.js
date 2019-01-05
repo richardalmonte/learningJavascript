f9784e37119df5fd51613fc9918bde76ad0d8c12
Richard Almonte <richard_almonte@hotmail.com>
2019-01-05 01:06:47 +0100

    Handled remaining bills

--- Atm/atm.js
+++ Atm/atm.js
@@ -1,19 +1,16 @@
-class Bill {
-    constructor(val, qty) {
-        this.value = val;
-        this.quantity = qty;
-        this.imageUrl = "img/us_" + val + ".jpg";
-    }
-}
 
 function giveMoneyAway() {
     var moneyText = document.getElementById("requested");
-    money = parseInt(moneyText.value);
+    var res = document.getElementById("result");
+    var money = parseInt(moneyText.value);
+    var withdrawed = [];
+    res.innerHTML = "";
+    var bills = 0;
 
     for (var bi of cashList) {
-        console.log(bi);
+
         if (money > 0) {
-            div = Math.floor(money / bi.value);
+            var div = Math.floor(money / bi.value);
             if (div > bi.quantity) {
                 bills = bi.quantity;
             }
@@ -24,22 +21,28 @@ function giveMoneyAway() {
             money = money - (bi.value * bills);
         }
     }
-    var res = document.getElementById("result");
     if (money > 0) {
         res.innerHTML = "No enough money";
     } else {
         for (var w of withdrawed) {
             if (w.quantity > 0) {
                 res.innerHTML += w.quantity + " Bills of $" + w.value + "<img src=\"" + w.imageUrl + "\"/>" + "<br/>";
+                substractBillsFromCash(w.value, w.quantity);
             }
         }
     }
 }
 
+function substractBillsFromCash(billValue, qty) {
+    for (var cash of cashList) {
+        if (cash.value == billValue) {
+            cash.quantity -= qty;
+            break;
+        }
+    }
+}
+
 var cashList = [];
-var withdrawed = [];
-
-
 cashList.push(new Bill(100, 10));
 cashList.push(new Bill(50, 10));
 cashList.push(new Bill(20, 30));
@@ -47,10 +50,6 @@ cashList.push(new Bill(10, 2));
 cashList.push(new Bill(5, 2));
 cashList.push(new Bill(2, 2));
 cashList.push(new Bill(1, 2));
-
-var money = 0;
-var div = 0;
-var bills = 0;
 
 var getMoneyButton = document.getElementById("giveMoney");
 getMoneyButton.addEventListener("click", giveMoneyAway);
---
+++ Atm/bill.js
@@ -0,0 +1,7 @@
+class Bill {
+    constructor(val, qty) {
+        this.value = val;
+        this.quantity = qty;
+        this.imageUrl = "img/us_" + val + ".jpg";
+    }
+}
